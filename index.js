9a1163c4dbb0886c57a47d6069593a06f92be66d
leparan <leparanfelix16@gmail.com>
2025-03-23 15:26:32 +0300

    sending-data-with-fetch

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(name, email) {
+    const formData = {
+      name: name,
+      email: email
+    };
+  
+    return fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    })
+    .then(response => response.json())
+    .then(object => {
+      document.body.innerHTML += `<p>ID: ${object.id}</p>`;
+    })
+    .catch(error => {
+      document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+    });
+  }
+  
